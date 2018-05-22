46a164cb8935284e2590b24d7912d70bdc48b8f1
zhangyunlong <496933982@qq.com>
2018-05-22 21:40:55 +0800

    first commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import config.GlobalCorsConfig;
+import controller.RealTimeController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableAutoConfiguration
+@Import(GlobalCorsConfig.class)
+public class Demo1Application implements EmbeddedServletContainerCustomizer{
+
+	@Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        
+        container.setPort(8007);
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeController.class, args);
+//		SpringApplication.run(AddDeviceController.class, args);
+//
+//		SpringApplication.run(MeasureController.class, args);
+//
+//		SpringApplication.run(DeviceDetailController.class, args);
+
+	}
+}
